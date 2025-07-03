bce6dd227be1a2478d584dfc33148716edebf5bb
SriHariPrasathR <118532333+SrihariPrasathR@users.noreply.github.com>
2025-07-03 15:02:47 +0530

    Create ValidParentheses.cpp

---
+++ ValidParentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stk;
+        for(char ch:s){
+            if(ch=='('||ch=='{'||ch=='[')
+            stk.push(ch);
+            else{
+                if(stk.empty()) return false;
+            if(ch==')'&&stk.top()!='('||ch=='}'&&stk.top()!='{'||ch==']'&&stk.top()!='[')
+           return false;
+            stk.pop();
+            }
+        }
+       return stk.empty(); 
+    }
+};
